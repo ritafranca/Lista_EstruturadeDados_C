b2e49371672bb50fede78b1c5709b2d4de6da1da
Rita <117872666+ritafranca@users.noreply.github.com>
2023-04-26 19:08:15 -0300

    Atualizar o Questao5.c

--- Questao5.c
+++ Questao5.c
@@ -2,40 +2,38 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-#define MAX_SIZE 100
-
 typedef struct { //implementando uma fila para armazenar a sequência de caracteres digitada pelo usuário
-    int inicio, fim;
-    char caracteres[MAX_SIZE];
+    int inicio, fim, tamanho;
+    char* caracteres;
 } Fila;
 
 typedef struct { //implementando uma fila para armazenar a mesma sequência de caracteres mas em ordem inversa
-    int topo;
-    char caracteres[MAX_SIZE];
+    int topo, tamanho;
+    char* caracteres;
 } Pilha;
 
 void inicializarFila(Fila* f) { //recebe como parâmetro um ponteiro para uma pilha e inicializa seu topo com -1 pra indicar que a pilha está vazia
     f->inicio = f->fim = -1;
+    f->tamanho = 0;
+    f->caracteres = NULL;
 }
 
 int filaVazia(Fila* f) { //recebe um ponteiro para uma pilha e verifica se ela está vazia quie é quando o topo da pilha é igual a -1
     return (f->inicio == -1);
 }
 
-int filaCheia(Fila* f) { //recebe um ponteiro para uma pilha e verifica se ela está cheia q é quando o topo da pilha é igual a MAX_SIZE - 1
-    return ((f->fim + 1) % MAX_SIZE == f->inicio);
-}
-
 void enfileirar(Fila* f, char caractere) {
-    if (filaCheia(f)) {
-        printf("Fila cheia\n");
-        exit(1);
+    if (filaVazia(f)) {
+        f->caracteres = (char*)malloc(sizeof(char));
+        f->caracteres[0] = caractere;
+        f->inicio = f->fim = 0;
+        f->tamanho = 1;
     } else {
-        if (filaVazia(f)) {
-            f->inicio = f->fim = 0;
-        } else {
-            f->fim = (f->fim + 1) % MAX_SIZE;
+        if (f->fim == f->tamanho - 1) {
+            f->caracteres = (char*)realloc(f->caracteres, 2 * f->tamanho * sizeof(char));
+            f->tamanho *= 2;
         }
+        f->fim++;
         f->caracteres[f->fim] = caractere;
     }
 }
@@ -47,9 +45,10 @@ char desenfileirar(Fila* f) {
     } else {
         char caractere = f->caracteres[f->inicio];
         if (f->inicio == f->fim) {
+            free(f->caracteres);
             inicializarFila(f);
         } else {
-            f->inicio = (f->inicio + 1) % MAX_SIZE;
+            f->inicio++;
         }
         return caractere;
     }
@@ -57,53 +56,69 @@ char desenfileirar(Fila* f) {
 
 void inicializarPilha(Pilha* p) { //mesma logica da pilha
     p->topo = -1;
+    p->tamanho = 0;
+    p->caracteres = NULL;
 }
 
 int pilhaVazia(Pilha* p) {
     return (p->topo == -1);
 }
 
-int pilhaCheia(Pilha* p) {
-    return (p->topo == MAX_SIZE - 1);
-}
-
 void empilhar(Pilha* p, char caractere) {
-    if (pilhaCheia(p)) {
-        printf("Pilha cheia\n");
-        exit(1);
+    if (pilhaVazia(p)) {
+        p->caracteres = (char*)malloc(sizeof(char));
+        p->caracteres[0] = caractere;
+        p->topo = 0;
+        p->tamanho = 1;
     } else {
-        p->caracteres[++(p->topo)] = caractere;
-    }
+        if (p->topo == p->tamanho - 1) {
+            p->caracteres = (char*)realloc(p->caracteres, 2 * p->tamanho * sizeof(char));
+            p->tamanho *= 2;
+        }
+        p->topo++;
+        p->caracteres[p->topo] = caractere;
+}
 }
 
 char desempilhar(Pilha* p) {
-    if (pilhaVazia(p)) {
-        printf("Pilha vazia\n");
-        exit(1);
-    } else {
-        return p->caracteres[(p->topo)--];
-    }
+if (pilhaVazia(p)) {
+printf("Pilha vazia\n");
+exit(1);
+} else {
+char caractere = p->caracteres[p->topo];
+p->topo--;
+return caractere;
+}
 }
 
-int main() { //aqui na main um caractere é removido da fila usando a função desenfileirar e em seguida é removido o caractere correspondente do topo da pilha usando o desempilhar
-
-    Fila f;
-    Pilha p;
-    char caractere;
-
-    inicializarFila(&f);
-    inicializarPilha(&p);
+int main() {
+Fila fila;
+Pilha pilha;
+char caractere;
+inicializarFila(&fila);
+inicializarPilha(&pilha);
 
 printf("Digite uma sequencia de caracteres: ");
-while ((caractere = getchar()) != '\n') {
-    enfileirar(&f, caractere);
-    empilhar(&p, caractere);
+do {
+    scanf("%c", &caractere);
+    if (isalpha(caractere)) { //verifica se o caractere é uma letra
+        enfileirar(&fila, caractere); //adiciona o caractere na fila
+        empilhar(&pilha, caractere); //adiciona o caractere na pilha
+    }
+} while (caractere != '\n'); //le até o usuário pressionar enter
+
+printf("Caracteres na ordem digitada: ");
+while (!filaVazia(&fila)) {
+    printf("%c", desenfileirar(&fila)); //desenfileira e imprime cada caractere da fila
 }
-//o caractere removido da pilha é então impresso, e a sequência de caracteres resultante é a sequência digitada pelo usuario mas com a ordem dos caracteres invertida
-while (!filaVazia(&f)) {
-    caractere = desenfileirar(&f);
-    caractere = desempilhar(&p);
-    printf("%c", caractere);
+printf("\n");
+
+printf("Caracteres na ordem inversa: ");
+while (!pilhaVazia(&pilha)) {
+    printf("%c", desempilhar(&pilha)); //desempilha e imprime cada caractere da pilha
+}
+printf("\n");
+
+return 0;
 }
 
-}
